767e2d9ed06ac5f2ea5809754ec01ee93c752a9a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-14 04:24:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(array) {
   for (i = 0; i < array.length; i++) {
-    console.log()
+    console.log(`Welcome ${array[i]}! You are employee #1.`)
   }
   return array
 }
