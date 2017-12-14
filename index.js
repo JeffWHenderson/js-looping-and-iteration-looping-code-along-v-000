59f4d326065b8fd9bbe8f49a16b3bc1db626ee71
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-14 04:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // Code your solutions in this file
-function wrapGifts (gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
- 
-  return gifts;
-}
